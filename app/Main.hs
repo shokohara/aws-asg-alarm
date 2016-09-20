4974cbae13357eee49cc815a1ad1ab564953bfd6
shokohara <shokohara@users.noreply.github.com>
2016-09-20 22:18:39 +0900

    Generic

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import Control.Applicative
 import Control.Monad
 import Control.Lens hiding (Context)
-import Data.ByteString.Lazy.Char8
+import Data.ByteString.Lazy.Char8 hiding (drop)
 import Data.Aeson
 import Data.Aeson.Lens
+import Data.Aeson.TH
+import GHC.Generics
 
-data Record = Record { eventVersion :: String } deriving Show
-data Event = Event { records :: [Record] } deriving Show
-data Context = Context { callbackWaitsForEmptyEventLoop :: Bool } deriving Show
-data Root = Root { event :: Event, context :: Context } deriving Show
+data Record = Record { eEventVersion :: String } deriving (Show, Generic)
+data Event = Event { rRecords :: [Record] } deriving (Show, Generic)
+data Context = Context { callbackWaitsForEmptyEventLoop :: Bool } deriving (Show, Generic)
+data Root = Root { event :: Event, context :: Context } deriving (Show, Generic)
 
+-- deriveJSON defaultOptions { fieldLabelModifier = drop 1 } ''Record
+-- deriveJSON defaultOptions { fieldLabelModifier = drop 1 } ''Event
 instance FromJSON Record where
-  parseJSON (Object v) = Record <$> v .: "EventVersion"
-  parseJSON _ = mempty
-
+  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 1 }
 instance FromJSON Event where
-  parseJSON (Object v) = Event <$> v .: "Records"
-  parseJSON _ = mempty
-
-instance FromJSON Context where
-  parseJSON (Object v) = Context <$> v .: "callbackWaitsForEmptyEventLoop"
-  parseJSON _ = mempty
-
-instance FromJSON Root where
-  parseJSON (Object v) = Root <$> v .: "event" <*> v .: "context"
-  parseJSON _ = mempty
+  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 1 }
+instance FromJSON Context
+instance FromJSON Root
 
 test2 x = decode x :: Maybe Event
 test = print . test2 =<< Data.ByteString.Lazy.Char8.readFile "event.json"
